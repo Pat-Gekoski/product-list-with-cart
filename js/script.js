998aab8e57779f01973b17baf12dfbbf46a23240
Patric Gekoski <=>
2025-01-20 14:31:06 -0500

    Add checkout modal

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ const addButtons = document.querySelectorAll('.add-btn')
 const totalItems = document.getElementById('total-items')
 const overlay = document.getElementById('overlay')
 const confirmModal = document.getElementById('confirmModal')
+const newOrderBtn = document.getElementById('newOrder')
 
-const menuItems = [
+let menuItems = [
 	{ id: 1, img: 'img/image1.jpg', name: 'Waffle with Berries', cost: 6.5, qty: 0 },
 	{ id: 2, img: 'img/image2.jpg', name: 'Vanilla Bean Crème Brûlée', cost: 7.0, qty: 0 },
 	{ id: 3, img: 'img/image3.jpg', name: 'Macaron Mix of Five', cost: 8.0, qty: 0 },
@@ -83,6 +84,17 @@ function removeFromCart(itemId) {
 	}
 
 	updateCartUI()
+}
+
+function resetButtons() {
+	for (const el of activeCartButtons) {
+		el.classList.remove('flex')
+		el.classList.add('hidden')
+	}
+	for (const el of emptyCartButtons) {
+		el.classList.remove('hidden')
+		el.classList.add('flex')
+	}
 }
 
 function removeAllItemsOfKind(e) {
@@ -167,7 +179,7 @@ function updateCartUI() {
 		list.classList.add('flex')
 
 		cartHtml += `
-			<div class="my-4 flex items-center justify-between">
+			<div class="my-8 flex items-center justify-between">
 					<p class="text-rose900 text-sm font-thin">Order Total</p>
 					<p class="text-rose900 text-2xl font-bold">$${totalCost.toFixed(2)}</p>
 				</div>
@@ -219,12 +231,81 @@ function addRemoveListeners() {
 }
 
 function openModal() {
+	let totalCost = 0
+	let orderHtml = ''
+
+	console.log('Update cart')
+
+	for (const item of menuItems) {
+		if (item.qty > 0) {
+			totalCost += item.qty * item.cost
+
+			orderHtml += `
+				<div class="flex items-center justify-between gap-3 mb-3">
+					<div class="w-[48px] h-[48px] overflow-hidden rounded-xl">
+						<img src="${item.img}" alt="${item.name}">
+					</div>
+					<div class="flex flex-col flex-1 justify-between gap-1">
+						<p class="text-sm text-rose900 font-semibold">${item.name}</p>
+						<div class="flex flex-row gap-3 items-center">
+							<p class="text-sm text-red font-semibold">${item.qty}x</p>
+							<p class="text-sm text-rose500 font-thin">@ $${item.cost.toFixed(2)}</p>
+						</div>
+					</div>
+					<p class="text-base text-rose900 font-semibold">$${(item.qty * item.cost).toFixed(2)}</p>
+				</div>
+			`
+		}
+	}
+
+	orderHtml += `
+		 <div class="flex items-center justify-between my-4 mt-5">
+			<p class="text-sm text-rose900 font-thin">Order Total</p>
+			<p class="text-2xl font-bold text-rose900">$${totalCost.toFixed(2)}</p>
+		</div>
+	`
+
+	const list = document.getElementById('modalOrderList')
+	list.innerHTML = orderHtml
+
 	overlay.classList.remove('opacity-0', 'pointer-events-none')
+	overlay.classList.add('opacity-100', 'pointer-events-auto')
 
 	setTimeout(() => {
-		confirmModal.classList.remove('translate-y-full')
-		confirmModal.classList.add('translate-y-0')
-	}, 100)
+		confirmModal.classList.remove('translate-y-[2000px]')
+		if (window.innerWidth >= 640) {
+			confirmModal.classList.add('translate-x-[-50%]', 'translate-y-[50%]')
+		} else {
+			confirmModal.classList.add('translate-y-0')
+		}
+	}, 300)
 }
 
-openModal()
+function hideModal() {
+	confirmModal.classList.remove('translate-y-0')
+	confirmModal.classList.remove('translate-x-[-50%]', 'translate-y-[50%]')
+	confirmModal.classList.add('translate-y-[2000px]')
+
+	menuItems.forEach((item) => (item.qty = 0))
+
+	setTimeout(() => {
+		overlay.classList.add('opacity-0', 'pointer-events-none')
+		overlay.classList.remove('opacity-100', 'pointer-events-auto')
+		updateCartUI()
+		resetButtons()
+	}, 300)
+}
+
+newOrderBtn.addEventListener('click', hideModal)
+
+window.addEventListener('resize', () => {
+	if (window.innerWidth >= 640) {
+		if (!confirmModal.classList.contains('hidden')) {
+			confirmModal.classList.add('translate-x-[-50%]', 'translate-y-[50%]')
+		}
+	} else {
+		if (!confirmModal.classList.contains('hidden')) {
+			confirmModal.classList.remove('translate-x-[-50%]', 'translate-y-[50%]')
+		}
+	}
+})
